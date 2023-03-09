32ff58ce59023f3ce0bec31cbf83df6d42a238d8
noelo <noel.oconnor@gmail.com>
2023-03-09 17:07:34 +0000

    {WIP} added tensorflow to requirements

--- battery-rul-estimation/experiments/unibo-powertools/file-download-task.py
+++ battery-rul-estimation/experiments/unibo-powertools/file-download-task.py
@@ -6,7 +6,7 @@ from kubernetes.client import V1Volume, V1SecretVolumeSource, V1VolumeMount, V1E
 from typing import NamedTuple
 
 # Download files to workspace
-def writeFiles(preppedData: OutputPath()):
+def readyData(preppedData: OutputPath()):
     import boto3
     import os
     import sys
@@ -16,90 +16,157 @@ def writeFiles(preppedData: OutputPath()):
     import shutil
     
 # export DEFAULT_ACCESSMODES=ReadWriteOnce
-# export DEFAULT_STORAGE_SIZE=2Gi
-# export DEFAULT_STORAGE_CLASS=kfp-csi-s3
+# export DEFAULT_STORAGE_SIZE=5Gi
+# export DEFAULT_STORAGE_CLASS=odf-lvm-vg1
     
-    # reload(logging)
-    # logging.basicConfig(format='%(asctime)s [%(levelname)s]: %(message)s', level=logging.DEBUG, datefmt='%Y/%m/%d %H:%M:%S')    
-        
-    clientArgs = {
-        'aws_access_key_id': 'minio',
-        'aws_secret_access_key': 'minio123',
-        'endpoint_url': 'http://minio-api.minio-dev.svc.cluster.local:9000',
-        'verify': False
-    }
+    reload(logging)
+    logging.basicConfig(format='%(asctime)s [%(levelname)s]: %(message)s', level=logging.DEBUG, datefmt='%Y/%m/%d %H:%M:%S')    
 
-    client = boto3.resource("s3", **clientArgs)
+    from data_processing.unibo_powertools_data import UniboPowertoolsData, CycleCols
+    from data_processing.model_data_handler import ModelDataHandler
+    from data_processing.prepare_rul_data import RulHandler
     
-    try:
-        print('Retrieving buckets...')
-        print()
-
-        for bucket in client.buckets.all():
-            bucket_name = bucket.name
-            print('Bucket name: {}'.format(bucket_name))
-
-            objects = client.Bucket(bucket_name).objects.all()
-
-            for obj in objects:
-                object_name = obj.key
-
-                print('Object name: {}'.format(object_name))
-
-            print()
-
-        print("Starting downloading file")
-        # client.Bucket('battery-data').download_file( 'unibo-powertools-dataset/README.md', datafiles)
-        print("Starting downloading file....done")
-        basepath = '/mnt'
-        with os.scandir(basepath) as entries:
-            for entry in entries:
-                print(entry.name)                     
-                
-    except ClientError as err:
-        print("Error: {}".format(err))
-        
-    # IS_TRAINING = False
-    # RESULT_NAME = ""
-    # IS_OFFLINE = True
-
-    # # if IS_OFFLINE:
-    # #     import plotly.offline as pyo
-    # #     pyo.init_notebook_mode()   
-
-
-    # from data_processing.unibo_powertools_data import UniboPowertoolsData, CycleCols
-    # from data_processing.model_data_handler import ModelDataHandler
-    # from data_processing.prepare_rul_data import RulHandler
+    data_path = "/mnt/"    
+    sys.path.append(data_path)
+    print(sys.path)
     
-    # data_path = "/mnt/"    
-    # sys.path.append(data_path)
-    # print(sys.path)
-    
-    # dataset = UniboPowertoolsData(
-    #     test_types=[],
-    #     chunk_size=1000000,
-    #     lines=[37, 40],
-    #     charge_line=37,
-    #     discharge_line=40,
-    #     base_path=data_path
-    # )
-    
-    # with open(preppedData, "b+w") as f:   
-    #     pickle.dump(dataset,f)
-    
-    # shutil.copyfile("/mnt/large_bin_file.dat",preppedData)
-    ba = bytearray(os.urandom(1_000_000))
+    dataset = UniboPowertoolsData(
+        test_types=[],
+        chunk_size=1000000,
+        lines=[37, 40],
+        charge_line=37,
+        discharge_line=40,
+        base_path=data_path
+    )
     
     with open(preppedData, "b+w") as f:   
-        f.write(ba)
+        pickle.dump(dataset,f)    
+    print('PrepData written...')
     
-    print('Files written...')
+def fitNormaliseData(preppedData: InputPath(),modelData:OutputPath()):
+    import boto3
+    import os
+    import sys
+    import logging
+    import pickle
+    from importlib import reload
+    import shutil
+    reload(logging)
+    logging.basicConfig(format='%(asctime)s [%(levelname)s]: %(message)s', level=logging.DEBUG, datefmt='%Y/%m/%d %H:%M:%S')    
+
+    from data_processing.unibo_powertools_data import UniboPowertoolsData, CycleCols
+    from data_processing.model_data_handler import ModelDataHandler
+    from data_processing.prepare_rul_data import RulHandler
+    
+    f = open(preppedData,"b+r")
+    dataset = pickle.load(f)
+        
+    train_names = [
+        '000-DM-3.0-4019-S',#minimum capacity 1.48
+        '001-DM-3.0-4019-S',#minimum capacity 1.81
+        '002-DM-3.0-4019-S',#minimum capacity 2.06
+        '009-DM-3.0-4019-H',#minimum capacity 1.41
+        '010-DM-3.0-4019-H',#minimum capacity 1.44
+        '014-DM-3.0-4019-P',#minimum capacity 1.7
+        '015-DM-3.0-4019-P',#minimum capacity 1.76
+        '016-DM-3.0-4019-P',#minimum capacity 1.56
+        '017-DM-3.0-4019-P',#minimum capacity 1.29
+        #'047-DM-3.0-4019-P',#new 1.98
+        #'049-DM-3.0-4019-P',#new 2.19
+        '007-EE-2.85-0820-S',#2.5
+        '008-EE-2.85-0820-S',#2.49
+        '042-EE-2.85-0820-S',#2.51
+        '043-EE-2.85-0820-H',#2.31
+        '040-DM-4.00-2320-S',#minimum capacity 3.75, cycles 188
+        '018-DP-2.00-1320-S',#minimum capacity 1.82
+        #'019-DP-2.00-1320-S',#minimum capacity 1.61
+        '036-DP-2.00-1720-S',#minimum capacity 1.91
+        '037-DP-2.00-1720-S',#minimum capacity 1.84
+        '038-DP-2.00-2420-S',#minimum capacity 1.854 (to 0)
+        '050-DP-2.00-4020-S',#new 1.81
+        '051-DP-2.00-4020-S',#new 1.866 
+    ]
+
+    test_names = [
+        '003-DM-3.0-4019-S',#minimum capacity 1.84
+        '011-DM-3.0-4019-H',#minimum capacity 1.36
+        '013-DM-3.0-4019-P',#minimum capacity 1.6
+        '006-EE-2.85-0820-S',# 2.621    
+        '044-EE-2.85-0820-H',# 2.43
+        '039-DP-2.00-2420-S',#minimum capacity 1.93
+        '041-DM-4.00-2320-S',#minimum capacity 3.76, cycles 190
+    ]
+
+    # %%
+    dataset.prepare_data(train_names, test_names)
+    dataset_handler = ModelDataHandler(dataset, [
+        CycleCols.VOLTAGE,
+        CycleCols.CURRENT,
+        CycleCols.TEMPERATURE
+    ])
+
+    rul_handler = RulHandler()
+    # %%
+    (train_x, train_y_soh, test_x, test_y_soh,
+    train_battery_range, test_battery_range,
+    time_train, time_test, current_train, current_test) = dataset_handler.get_discharge_whole_cycle_future(train_names, test_names)
+
+    train_x = train_x[:,:284,:]
+    test_x = test_x[:,:284,:]
+
+    x_norm = rul_handler.Normalization()
+    train_x, test_x = x_norm.fit_and_normalize(train_x, test_x) 
+    
+    dataStore = [train_x, train_y_soh, test_x, test_y_soh,train_battery_range, test_battery_range,time_train, time_test, current_train, current_test]  
+    
+    with open(modelData, "b+w") as f:   
+        pickle.dump(dataStore,f)    
+    print('modelData written...')
+    
+def autoEncodeData(modelData: InputPath()):
+    import tensorflow as tf
+    from tensorflow import keras
+    from tensorflow.keras import layers, regularizers
+    from tensorflow.keras.models import Model
+    import logging
+    import pickle
+    from importlib import reload
+    reload(logging)
+    logging.basicConfig(format='%(asctime)s [%(levelname)s]: %(message)s', level=logging.DEBUG, datefmt='%Y/%m/%d %H:%M:%S')    
+
+    from data_processing.unibo_powertools_data import UniboPowertoolsData, CycleCols
+    from data_processing.model_data_handler import ModelDataHandler
+    from data_processing.prepare_rul_data import RulHandler
+    
+    f = open(modelData,"b+r")
+    dataStore = pickle.load(f)
+    
+    train_x=dataStore[0]
+    train_y_soh=dataStore[1]
+    test_x=dataStore[2]
+    test_y_soh=dataStore[3]
+    train_battery_range=dataStore[4]
+    test_battery_range=dataStore[5]
+    time_train=dataStore[6]
+    time_test=dataStore[7]
+    current_train=dataStore[8]
+    current_test=dataStore[9]  
+    
+    # print("cut train shape {}".format(train_x.shape))
+    # print("cut test shape {}".format(test_x.shape))
+  
     
     
-    
-writeFiles_op= components.create_component_from_func(
-    writeFiles, base_image='registry.access.redhat.com/ubi8/python-38',
+readyData_op= components.create_component_from_func(
+    readyData, base_image='quay.io/noeloc/batterybase',
+    packages_to_install=['boto3'])
+
+fitNormaliseData_op= components.create_component_from_func(
+    fitNormaliseData, base_image='quay.io/noeloc/batterybase',
+    packages_to_install=['boto3'])
+
+autoEncodeData_op= components.create_component_from_func(
+    autoEncodeData, base_image='quay.io/noeloc/batterybase',
     packages_to_install=['boto3'])
 
 
@@ -128,8 +195,12 @@ def download_and_store():
         persistent_volume_claim=V1PersistentVolumeClaimVolumeSource(
             claim_name='batterydata',)
         )
-    res = writeFiles_op().add_pvolumes({"/mnt": vol}).add_pod_label('pipelines.kubeflow.org/cache_enabled', 'false')
-    readFiles_op(res.output).add_pod_annotation(name="tekton.dev/output_artifacts", value=([])).add_pod_annotation(name="tekton.dev/artifact_items", value=([])).add_pod_label('pipelines.kubeflow.org/cache_enabled', 'false')
+    res = readyData_op().add_pvolumes({"/mnt": vol})
+    prep = fitNormaliseData_op(res.output)
+    autoEncodeData_op(prep.output)
+    
+    # .add_pod_label('pipelines.kubeflow.org/cache_enabled', 'false')
+    # readFiles_op(prep.output).add_pod_annotation(name="tekton.dev/output_artifacts", value=([])).add_pod_annotation(name="tekton.dev/artifact_items", value=([])).add_pod_label('pipelines.kubeflow.org/cache_enabled', 'false')
     
     # print('Pipeline Completed...',x.outputs['p1'])
     
